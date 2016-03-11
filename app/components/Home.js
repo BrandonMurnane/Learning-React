54f2be110cf5e5d8fc42d641ad20e7362c902997
BrandonMurnane <C13377256@mydit.ie>
2016-03-11 17:08:39 +0000

    test

--- app/components/Home.js
+++ app/components/Home.js
@@ -8,7 +8,7 @@ var Hello = React.createClass({
     return (
           <MainContainer>
           	<h1>Github Battle</h1>
-      	<p className='lead'> Some fancy motto </p>
+      	<p className='lead'> Lets Get Ready to Rumble !</p>
       	<Link to='/playerOne'>
       		<button type ='button' className='btn btn-lg btn-success'>
       			Get Started
